9e9ce549099c5a3dd3ca4a14947cee7c2739b889
Prakash4844 <pk484442@gmail.com>
2023-04-29 01:33:53 +0530

    Completed Day-19

--- Day-19/Turtle_Race.py
+++ Day-19/Turtle_Race.py
@@ -6,16 +6,39 @@ screen = Screen()
 screen.setup(width=1000, height=500)
 
 bet = screen.textinput("Make your bet.", "Which color turtle will win the race?:")
-tim = Turtle()
-tim.shape("turtle")
-tim.penup()
-tim.goto(x=-450, y=-200)
 
-print(bet)
+fin_tur = Turtle("turtle")
+fin_tur.hideturtle()
+fin_tur.penup()
+fin_tur.goto(x=450, y=250)
+fin_tur.right(90)
+fin_tur.pendown()
+fin_tur.forward(520)
+turtles = []
+y = -120
+is_race_on = False
 
+for color in colors:
+    new_tur = Turtle(shape="turtle")
+    new_tur.penup()
+    new_tur.color(color)
+    new_tur.goto(-450, y)
+    y += 40
+    turtles.append(new_tur)
 
+if bet:
+    is_race_on = True
 
+while is_race_on:
+    for tur in turtles:
+        tur.forward(random.randint(0, 10))
+        if tur.xcor() >= 435:
+            print(tur.pencolor(), "turtle Have won the race")
+            is_race_on = False
 
-
+if bet == tur:
+    print("Congrats, You won the bet!!.")
+else:
+    print("Your Turtle lost the bet, Better luck next time!")
 
 screen.exitonclick()
