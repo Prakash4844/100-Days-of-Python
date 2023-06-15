ed49427225185fdde3b4c22ef612320b1c966d18
Prakash4844 <pk484442@gmail.com>
2023-06-15 15:23:22 +0530

    Done Half the stuff

---
+++ Day-28/ReadMe.md
@@ -0,0 +1,20 @@
+## Day 28
+
+    Goal: Things we will learn in Python.
+        - GUI with Tkinter
+        - Tkinter canvas
+        - Dynamic Typing
+
+### What will be Make Today?
+    - Pomodore Timer App
+
+
+---
+- [Color-hunt](https://colorhunt.co/)
+- [TCL tk Docs](http://tcl.tk/man/tcl8.6/TclCmd/after.htm)
+- [Dynamic typing stackoverflow](https://stackoverflow.com/questions/11328920/is-python-strongly-typed)
+
+---
+### My Files
+
+- [Pomodoro](main.py)
---
+++ Day-28/main.py
@@ -0,0 +1,61 @@
+import math
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    count_down(5 * 60)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    start_button.config(state="disabled")
+    canvas.itemconfig(timer_text, text=f"{math.floor(count/60)}:{round(count%60, 2)}")
+    if count > 0:
+        app.after(1000, count_down, count - 1)
+    else:
+        start_button.config(state="normal")
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+app = Tk()
+app.title("Pomodoro")
+app.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 26, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 36, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", font=(FONT_NAME, 20), fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+exit_button = Button(text="Exit", highlightthickness=0, command=app.quit)
+exit_button.grid(column=1, row=4)
+
+
+app.mainloop()
