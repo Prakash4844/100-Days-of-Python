27ead6067dbfef9f91e35c471b75968b9810b43f
Prakash4844 <pk484442@gmail.com>
2023-06-16 10:13:10 +0530

    Completed Day-28

--- Day-28/main.py
+++ Day-28/main.py
@@ -1,5 +1,6 @@
 import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,14 +10,38 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    start_button.config(state="normal")
+    app.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_MIN * 60)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN * 60)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(WORK_MIN * 60)
+        timer_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
@@ -28,11 +53,19 @@ def count_down(count):
     else:
         count_sec = (count % 60)
 
-    canvas.itemconfig(timer_text, text=f"{math.floor(count/60)}:{count_sec}")
+    canvas.itemconfig(timer_text, text=f"{math.floor(count / 60)}:{count_sec}")
     if count > 0:
-        app.after(1000, count_down, count - 1)
+        global timer
+        timer = app.after(1000, count_down, count - 1)
     else:
-        start_button.config(state="normal")
+        # start_button.config(state="normal")
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -53,14 +86,13 @@ timer_label.grid(column=1, row=0)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", font=(FONT_NAME, 20), fg=GREEN, bg=YELLOW)
+check_marks = Label(font=(FONT_NAME, 20), fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 exit_button = Button(text="Exit", highlightthickness=0, command=app.quit)
 exit_button.grid(column=1, row=4)
-
 
 app.mainloop()
