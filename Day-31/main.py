08e46c2124f163c5ff37013b4943b9109b394e13
Prakash4844 <pk484442@gmail.com>
2023-07-11 14:46:47 +0530

    Day-31 WIP

--- Day-31/main.py
+++ Day-31/main.py
@@ -1,6 +1,33 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+JP_WORD = "Eng-Jp.csv"
+HI_WORD = "Eng-Hi.csv"
+
+# ---------------------------- Read CSV data ------------------------------- #
+
+data = pandas.read_csv(HI_WORD)
+to_learn_dict = data.to_dict(orient="records")
+random_word = random.choice(to_learn_dict)
+random_hindi_word = random_word["Hindi"]
+
+# ---------------------------- Random Hindi word ------------------------------- #
+
+
+def next_card():
+    """
+    Get random Hindi word from the CSV file
+    :return: Random Hindi word
+    """
+    global random_word, random_hindi_word
+    random_word = random.choice(to_learn_dict)
+    random_hindi_word = random_word["Hindi"]
+    canvas.itemconfig(card_front, image=card_front_img)
+    canvas.itemconfig(language_text, text="Hindi", fill="black")
+    canvas.itemconfig(word_text, text=f"{random_hindi_word}", fill="black")
+
 
 # ---------------------------- UI ------------------------------- #
 app = Tk()
@@ -13,17 +40,19 @@ card_back_img = PhotoImage(file="images/card_back.png")
 card_front = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=0, columnspan=2)
 
-language_text = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+language_text = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, width=98, height=98, highlightthickness=0,
+                      borderwidth=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, width=98, height=99, highlightthickness=0,
+                      borderwidth=0, command=next_card)
 right_button.grid(row=1, column=1)
 
-app.mainloop()
+next_card()
 
-# ---------------------------- Logic ------------------------------- #
+app.mainloop()
