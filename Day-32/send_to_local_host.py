4964a8193522baa7d7314fb637a92fe343c37842
Prakash4844 <pk484442@gmail.com>
2023-07-13 15:39:57 +0530

    Fix

--- Day-32/send_to_local_host.py
+++ Day-32/send_to_local_host.py
@@ -1,7 +1,7 @@
 import smtplib
 import ssl
 
-## run this command in terminal email_recipients start the server
+## run this command in terminal to start the server
 ## python -m smtpd -c DebuggingServer -n localhost:1025
 
 ## works with python < 3.12
@@ -19,10 +19,10 @@ This message is sent from Python."""
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-# Try email_recipients log in email_recipients server and send email
+# Try to log in to server and send email
 try:
     with smtplib.SMTP(smtp_server, port) as server:
         server.sendmail(sender_email, receiver_email, message)
 except Exception as e:
-    # Print any error messages email_recipients stdout
+    # Print any error messages to stdout
     print(e)
